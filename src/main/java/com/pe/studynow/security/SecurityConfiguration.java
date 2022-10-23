12f83e693db16abd3954095e4ec37febd0671e13
Sergio Nicolas Huaman Cheng <sergiohuamanch15@gmail.com>
2022-10-23 17:04:07 -0500

    changes in .css and security

--- src/main/java/com/pe/studynow/security/SecurityConfiguration.java
+++ src/main/java/com/pe/studynow/security/SecurityConfiguration.java
@@ -39,41 +39,50 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	//Restringir acceso a la pagina
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
-		// Aqui realiza la configuración de los permisos
-				.antMatchers("/students").hasRole("ADMINISTRATOR")
-				.antMatchers("/teachers").hasRole("ADMINISTRATOR")
-				.antMatchers("/careers").hasRole("ADMINISTRATOR")
-				.antMatchers("/sections").hasRole("ADMINISTRATOR")
-				.antMatchers("/courses").hasRole("ADMINISTRATOR")
-				.antMatchers("/users").hasRole("ADMINISTRATOR")
+		try {
+			http.authorizeRequests()
+			// Aqui realiza la configuración de los permisos
 
-				.antMatchers("/students/new").hasRole("ADMINISTRATOR")
-				.antMatchers("/teachers/new").hasRole("ADMINISTRATOR")
-				.antMatchers("/careers/new").hasRole("ADMINISTRATOR")
-				.antMatchers("/sections/new").hasRole("ADMINISTRATOR")
-				.antMatchers("/courses/new").hasRole("ADMINISTRATOR")
-				.antMatchers("/users/new").hasRole("ADMINISTRATOR")
+					.antMatchers("/*.js", "/*.css").permitAll()
+			
+					.antMatchers("/students").hasRole("ADMINISTRATOR")
+					.antMatchers("/teachers").hasRole("ADMINISTRATOR")
+					.antMatchers("/careers").hasRole("ADMINISTRATOR")
+					.antMatchers("/sections").hasRole("ADMINISTRATOR")
+					.antMatchers("/courses").hasRole("ADMINISTRATOR")
+					.antMatchers("/users").hasRole("ADMINISTRATOR")
 
-				.antMatchers("/users/view-profile").hasAnyRole("ADMINISTRATOR","TEACHER","STUDENT")
-				
-				.antMatchers("/students/**/edit").hasRole("ADMINISTRATOR")
-				.antMatchers("/teachers/**/edit").hasRole("ADMINISTRATOR")
-				.antMatchers("/careers/**/edit").hasRole("ADMINISTRATOR")
-				.antMatchers("/sections/**/edit").hasRole("ADMINISTRATOR")
-				.antMatchers("/courses/**/edit").hasRole("ADMINISTRATOR")
-				.antMatchers("/users/**/edit").hasRole("ADMINISTRATOR")
-				
-				.antMatchers("/students/**/edit").hasAnyAuthority("ACCESS_EDIT_MATRI", "ACCESS_ALL")
-				.antMatchers("/courses/list").hasRole("TEACHER")
-				.antMatchers("/enrollments/**").hasRole("STUDENT")
+					.antMatchers("/students/new").hasRole("ADMINISTRATOR")
+					.antMatchers("/teachers/new").hasRole("ADMINISTRATOR")
+					.antMatchers("/careers/new").hasRole("ADMINISTRATOR")
+					.antMatchers("/sections/new").hasRole("ADMINISTRATOR")
+					.antMatchers("/courses/new").hasRole("ADMINISTRATOR")
+					.antMatchers("/users/new").hasRole("ADMINISTRATOR")
 
-				.antMatchers("/enrollments").hasRole("STUDENT")
+					.antMatchers("/users/view-profile").hasAnyRole("ADMINISTRATOR","TEACHER","STUDENT")
+					
+					.antMatchers("/students/**/edit").hasRole("ADMINISTRATOR")
+					.antMatchers("/teachers/**/edit").hasRole("ADMINISTRATOR")
+					.antMatchers("/careers/**/edit").hasRole("ADMINISTRATOR")
+					.antMatchers("/sections/**/edit").hasRole("ADMINISTRATOR")
+					.antMatchers("/courses/**/edit").hasRole("ADMINISTRATOR")
+					.antMatchers("/users/**/edit").hasRole("ADMINISTRATOR")
+					
+					.antMatchers("/students/**/edit").hasAnyAuthority("ACCESS_EDIT_MATRI", "ACCESS_ALL")
+					.antMatchers("/courses/list").hasRole("TEACHER")
+					.antMatchers("/enrollments/**").hasRole("STUDENT")
 
-				.antMatchers("/main").hasAnyRole("ADMINISTRATOR","TEACHER","STUDENT")
-				.and()
-					.formLogin().loginPage("/login").permitAll().and().logout().permitAll().and()
-					.exceptionHandling().accessDeniedPage("/error");
+					.antMatchers("/enrollments").hasRole("STUDENT")
+
+					.antMatchers("/main").hasAnyRole("ADMINISTRATOR","TEACHER","STUDENT")
+					.and()
+						.formLogin().loginPage("/login").permitAll().and().logout().permitAll().and()
+						.exceptionHandling().accessDeniedPage("/error");
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.print(e.getMessage());
+		}
+		
 				
 	}
 	
