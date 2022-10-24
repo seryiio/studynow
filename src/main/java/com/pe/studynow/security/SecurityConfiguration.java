fb33ba1b5142f60ad35bf192058c6b426459cfe5
Sergio Nicolas Huaman Cheng <sergiohuamanch15@gmail.com>
2022-10-24 09:28:02 -0500

    add LoginSucessHandler

--- src/main/java/com/pe/studynow/security/SecurityConfiguration.java
+++ src/main/java/com/pe/studynow/security/SecurityConfiguration.java
@@ -17,74 +17,68 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	private MyUserDetailsService myUserDetailsService;
-	
+
 	@Bean
 	PasswordEncoder passwordEnconder() {
 		return new BCryptPasswordEncoder();
 	}
-	
+
 	DaoAuthenticationProvider authenticationProvider() {
 		DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
 		daoAuthenticationProvider.setUserDetailsService(myUserDetailsService);
-		daoAuthenticationProvider.setPasswordEncoder(passwordEnconder());		
-		
+		daoAuthenticationProvider.setPasswordEncoder(passwordEnconder());
+
 		return daoAuthenticationProvider;
 	}
+
+	@Autowired
+	private LoginSucessHandler sucessHandler;
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.authenticationProvider(authenticationProvider());
 	}
 
-	//Restringir acceso a la pagina
+	// Restringir acceso a la pagina
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		try {
 			http.authorizeRequests()
-			// Aqui realiza la configuración de los permisos
+					// Aqui realiza la configuración de los permisos
 
 					.antMatchers("/*.js", "/*.css").permitAll()
-			
-					.antMatchers("/students").hasRole("ADMINISTRATOR")
-					.antMatchers("/teachers").hasRole("ADMINISTRATOR")
-					.antMatchers("/careers").hasRole("ADMINISTRATOR")
-					.antMatchers("/sections").hasRole("ADMINISTRATOR")
-					.antMatchers("/courses").hasRole("ADMINISTRATOR")
-					.antMatchers("/users").hasRole("ADMINISTRATOR")
 
-					.antMatchers("/students/new").hasRole("ADMINISTRATOR")
-					.antMatchers("/teachers/new").hasRole("ADMINISTRATOR")
-					.antMatchers("/careers/new").hasRole("ADMINISTRATOR")
-					.antMatchers("/sections/new").hasRole("ADMINISTRATOR")
-					.antMatchers("/courses/new").hasRole("ADMINISTRATOR")
-					.antMatchers("/users/new").hasRole("ADMINISTRATOR")
+					.antMatchers("/students").hasRole("ADMINISTRATOR").antMatchers("/teachers").hasRole("ADMINISTRATOR")
+					.antMatchers("/careers").hasRole("ADMINISTRATOR").antMatchers("/sections").hasRole("ADMINISTRATOR")
+					.antMatchers("/courses").hasRole("ADMINISTRATOR").antMatchers("/users").hasRole("ADMINISTRATOR")
 
-					.antMatchers("/users/view-profile").hasAnyRole("ADMINISTRATOR","TEACHER","STUDENT")
-					
-					.antMatchers("/students/**/edit").hasRole("ADMINISTRATOR")
-					.antMatchers("/teachers/**/edit").hasRole("ADMINISTRATOR")
-					.antMatchers("/careers/**/edit").hasRole("ADMINISTRATOR")
-					.antMatchers("/sections/**/edit").hasRole("ADMINISTRATOR")
-					.antMatchers("/courses/**/edit").hasRole("ADMINISTRATOR")
-					.antMatchers("/users/**/edit").hasRole("ADMINISTRATOR")
-					
+					.antMatchers("/students/new").hasRole("ADMINISTRATOR").antMatchers("/teachers/new")
+					.hasRole("ADMINISTRATOR").antMatchers("/careers/new").hasRole("ADMINISTRATOR")
+					.antMatchers("/sections/new").hasRole("ADMINISTRATOR").antMatchers("/courses/new")
+					.hasRole("ADMINISTRATOR").antMatchers("/users/new").hasRole("ADMINISTRATOR")
+
+					.antMatchers("/users/view-profile").hasAnyRole("ADMINISTRATOR", "TEACHER", "STUDENT")
+
+					.antMatchers("/students/**/edit").hasRole("ADMINISTRATOR").antMatchers("/teachers/**/edit")
+					.hasRole("ADMINISTRATOR").antMatchers("/careers/**/edit").hasRole("ADMINISTRATOR")
+					.antMatchers("/sections/**/edit").hasRole("ADMINISTRATOR").antMatchers("/courses/**/edit")
+					.hasRole("ADMINISTRATOR").antMatchers("/users/**/edit").hasRole("ADMINISTRATOR")
+
 					.antMatchers("/students/**/edit").hasAnyAuthority("ACCESS_EDIT_MATRI", "ACCESS_ALL")
-					.antMatchers("/courses/list").hasRole("TEACHER")
-					.antMatchers("/enrollments/**").hasRole("STUDENT")
+					.antMatchers("/courses/list").hasRole("TEACHER").antMatchers("/enrollments/**").hasRole("STUDENT")
 
 					.antMatchers("/enrollments").hasRole("STUDENT")
 
-					.antMatchers("/main").hasAnyRole("ADMINISTRATOR","TEACHER","STUDENT")
-					.and()
-						.formLogin().loginPage("/login").permitAll().and().logout().permitAll().and()
-						.exceptionHandling().accessDeniedPage("/error");
+					.and().formLogin().successHandler(sucessHandler).loginPage("/login").loginProcessingUrl("/login")
+					// Si el login es exitoso, retorna a /home
+					.defaultSuccessUrl("/home").permitAll().and().logout().logoutSuccessUrl("/login").permitAll()
+					// Si el usuario va a una ruta sin acceso, devuelve a /error_403 (Configurado en MvcConfig)
+					.and().exceptionHandling().accessDeniedPage("/error403");
 		} catch (Exception e) {
 			// TODO: handle exception
 			System.out.print(e.getMessage());
 		}
-		
-				
+
 	}
-	
-	
+
 }
